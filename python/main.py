ebbb809d88263274915edaa014910717ee318c5d
zjfroot <zjfroot@gmail.com>
2009-05-11 17:55:45 +0200

    N/A

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 # -*- coding: iso-8859-15 -*-
 import sys
 import random
+from misc import Tools
 
 class sudoku:
     def new_game(self):
@@ -15,18 +16,22 @@ class sudoku:
                    [1,2,3,4,5,6,7,8,9]]
         self.print_sudoku(numbers)
 
-    ##todo:
-    ##generate distinct random numbers
-
-    ## sudoku: representation of the soduku
+    ## sudoku: a list to represent a soduku
     ## location: 0 1 2 ... 8, which block to populate
     ## limit: how many numbers to populate
     def populate_block(self,sudoku,location,limit):
-        pass 
+        t = Tools()
+        locations = t.r_locations([1,2,3,4,5,6,7,8,9],limit)
+        if location == 0:
+            pass
+        elif location == 1:
+            pass
+        else:
+            pass
 
     ## return the random number from a list
-    def r(self,within_list):
-        return random.choice(within_list) 
+    #def r(self,within_list):
+    #    return random.choice(within_list) 
 
     def print_sudoku(self,numbers):
         i = j = 0
@@ -44,13 +49,6 @@ class sudoku:
             i = 0
             j += 1
         print
-    
-    ## below are testing funcitons
-    def test_r(self):
-        kkk = 0
-        while kkk < 8:
-            print mysudoku.r([1,2,3,4,5,6,7,8,9])
-            kkk += 1
 
 ##         print "1 2 3  4 5 6  7 8 9"
 ##         print "1 2 3  4 5 6  7 8 9"
@@ -65,4 +63,4 @@ class sudoku:
 
 mysudoku = sudoku()
 ##mysudoku.new_game()
-mysudoku.test_r()
+##mysudoku.test_r()
--- python/test.py
+++ python/test.py
@@ -1,7 +1,7 @@
 from main import sudoku
+from misc import Tools
 
-
-class sudokuTester:
+class SudokuTester:
 
     def test_r(self):
         mysudoku = sudoku()
@@ -10,7 +10,18 @@ class sudokuTester:
             print mysudoku.r([1,2,3,4,5,6,7,8,9])
             kkk += 1
 
+    def test_tools_r(self):
+        tool = Tools()
+        kkk = 0
+        while kkk < 8:
+            print tool.r([1,2,3,4,5,6,7,8,9])
+            kkk += 1
 
-
-t = sudokuTester()
-t.test_r()
+    def test_tools_r_locations(self):
+        tool = Tools()
+        print tool.r_locations([1,2,3,4,5,6,7,8,9],4)
+        
+t = SudokuTester()
+#t.test_r()
+#t.test_tools_r()
+t.test_tools_r_locations()
